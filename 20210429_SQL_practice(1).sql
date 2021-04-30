30b03a2662420baeecf4581c8a399bcbc6580711
starfishkenny <starfish8913@naver.com>
2021-04-30 22:16:31 +0900

    updated sql script

--- 20210429_SQL_practice(1).sql
+++ 20210429_SQL_practice(1).sql
@@ -41,4 +41,28 @@ select first_name,last_name from customer where first_name = 'Jamie';
 ------------------------------------------------------------
 select first_name,last_name from customer where first_name = 'Jamie' and last_name = 'Rice';
 ------------------------------------------------------------
-select customer_id,amount,payment_id from customer where amount <= 1 or amount >= 8;
+select customer_id,amount,payment_id from payment where amount <= 1 or amount >= 8;
+------------------------------------------------------------
+select film_id,title,release_year from film order by film_id limit 5;
+------------------------------------------------------------
+select film_id,title,release_year from film order by film_id limit 4 offset 3;
+------------------------------------------------------------
+select film_id,title,rental_rate from film order by rental_rate desc limit 10;
+------------------------------------------------------------
+select film_id,title from film order by title fetch first row only;
+------------------------------------------------------------
+select film_id,title from film order by title fetch first 1 row only;
+------------------------------------------------------------
+select film_id,title from film order by title offset 5 rows fetch first 5 row only;
+------------------------------------------------------------
+select customer_id,rental_id,return_date from rental where customer_id in (1,2) order by return_date desc;
+------------------------------------------------------------
+select customer_id,rental_id,return_date from rental where customer_id = 1 or customer_id = 2 order by return_date desc;
+------------------------------------------------------------
+select customer_id,rental_id,return_date from rental where customer_id not in (1,2) order by return_date desc;
+------------------------------------------------------------
+select customer_id,rental_id,return_date from rental where customer_id<>1 and customer_id<>2 order by return_date desc;
+------------------------------------------------------------
+select customer_id from rental where cast (return_date as date) = '2005-05-27';
+------------------------------------------------------------
+select first_name,last_name from customer where customer_id in (select customer_id from rental where cast (return_date as date) = '2005-05-27');
