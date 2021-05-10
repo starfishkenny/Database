ff0f48bcd4932d8c2fb2cb43f3f62b9f11a48222
starfishkenny <starfish8913@naver.com>
2021-05-10 22:22:48 +0900

    updated sql

--- 20210505_SQL_practice(2).sql
+++ 20210505_SQL_practice(2).sql
@@ -376,3 +376,24 @@ select grouping(brand) grouping_brand,grouping(segment) grouping_segment,
 	(segment),
 	())
 	order by brand,segment;
+---------------------------------------------
+select brand,segment,sum(quantity) from sales
+	group by
+		rollup (brand,segment)
+	order by brand,segment;
+---------------------------------------------
+select brand,segment,sum(quantity) from sales
+	group by segment,
+		rollup (brand)
+	order by segment,brand;
+---------------------------------------------
+select brand,segment,sum(quantity) from sales
+	group by
+		cube (brand,segment)
+	order by brand,segment;
+---------------------------------------------
+select brand,segment,sum(quantity) from sales
+	group by brand,
+		cube (segment)
+	order by brand,segment;
+---------------------------------------------
