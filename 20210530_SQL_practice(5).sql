5c54f7960f953539b97811ef0eb3004fa3f2ca5e
starfishkenny <starfish8913@naver.com>
2021-06-03 17:06:34 +0900

    completed SQL query

--- 20210530_SQL_practice(5).sql
+++ 20210530_SQL_practice(5).sql
@@ -343,7 +343,7 @@ insert into PERSON_UNIQUE_INDEX_TEST(FIRST_NAME,LAST_NAME,EMAIL)
 values
 ('John','Doe','j.doe@postgresqltutorial.com'); -- 기존에 존재하는 이메일 주소를 입력하려고 함 --> 실패
 -----------------------------------------------------------
--- not null
+-- not null (특정 칼럼에 null 값이 들어가는 것을 방지)
 create table INVOICE
 (ID serial primary key,
 PRODUCT_ID int not null,
@@ -354,9 +354,101 @@ insert into INVOICE (PRODUCT_ID, QTY, NET_PRICE)
 values
 (null,1,1); -- not null 조건 위반 (PRODUCT_ID)
 -----------------------------------------------------------
+-- update & not null
+create table INVOICE_UPDATE_TEST
+(ID serial primary key,
+PRODUCT_ID int not null,
+QTY numeric not null check(QTY > 0),
+NET_PRICE numeric check(NET_PRICE > 0));
 
+insert into INVOICE_UPDATE_TEST (PRODUCT_ID, QTY, NET_PRICE)
+values
+(1,1,1);
+
+commit;
+
+update INVOICE_UPDATE_TEST
+set PRODUCT_ID = null
+	where PRODUCT_ID = 1;
 -----------------------------------------------------------
+-- 실습문제1
+create table TB_MOVIE_CUST
+(CUSTOMER_ID char(10) primary key,
+CUSTOMER_NAME varchar(50) not null,
+SEX varchar(6) not null check (SEX in ('남자','여자')),
+BIRTH_DATE date not null,
+ADDRESS varchar(200),
+PHONE_NUMBER varchar(13),
+CUSTOMER_GRADE char(1) not null check (CUSTOMER_GRADE in ('S','A','B','C','D')),
+JOIN_DATE date not null check (JOIN_DATE <= EXPIRE_DATE),
+EXPIRE_DATE date not null default to_date('9999-12-31','YYYY-MM-DD'));
+
+create table TB_MOVIE_RESV
+(RESERVATION_NUMB char(10) primary key,
+MOVIE_ID char(6) not null,
+THEATER_NUMB char(6) not null,
+CUSTOMER_ID varchar(10) not null references TB_MOVIE_CUST(CUSTOMER_ID),
+START_TIME timestamp not null check (START_TIME < END_TIME),
+END_TIME timestamp not null,
+SEAT_NUMB char(4) not null);
 -----------------------------------------------------------
+insert into TB_MOVIE_CUST
+(CUSTOMER_ID, CUSTOMER_NAME, SEX, BIRTH_DATE, ADDRESS, PHONE_NUMBER, CUSTOMER_GRADE,JOIN_DATE)
+values
+('0000000001', '이경오', '남자', TO_DATE('1984-06-12', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-1', '010-1234-1234', 'S', TO_DATE('2017-01-01', 'YYYY-MM-DD'))
+, ('0000000002', '홍길동', '남자', TO_DATE('1971-07-04', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-2', '010-4321-4321', 'A', TO_DATE('2018-06-01', 'YYYY-MM-DD'))
+, ('0000000003', '이수지', '여자', TO_DATE('1994-12-28', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-3', '010-5678-5678', 'B', TO_DATE('2019-12-01', 'YYYY-MM-DD'))
+;
+
+commit;
+
+select * from TB_MOVIE_CUST;
 -----------------------------------------------------------
+insert into TB_MOVIE_RESV
+values
+('9000000001', '000001', '000010', '0000000001', to_timestamp('2019-05-01 14:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2019-05-01 17:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'A-01')
+, ('9000000002', '000002', '000020', '0000000001', to_timestamp('2019-04-01 14:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2019-05-01 17:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'A-11')
+, ('9000000003', '000003', '000040', '0000000002', to_timestamp('2019-03-01 16:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2019-05-01 18:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'B-12')
+, ('9000000004', '000004', '000050', '0000000002', to_timestamp('2019-03-25 21:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2019-05-01 23:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'C-02')
+, ('9000000005', '000005', '000060', '0000000003', to_timestamp('2018-07-11 16:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2019-05-01 18:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'E-03')
+, ('9000000006', '000006', '000060', '0000000003', to_timestamp('2018-08-15 21:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2019-05-01 23:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'D-10')
+;
+
+commit;
+
+select * from TB_MOVIE_RESV;
 -----------------------------------------------------------
------------------------------------------------------------
+-- 실습문제2
+insert into TB_MOVIE_CUST(CUSTOMER_ID, CUSTOMER_NAME, SEX, BIRTH_DATE, ADDRESS, PHONE_NUMBER, CUSTOMER_GRADE,JOIN_DATE)
+values
+('0000000004', '이승우', '남자', TO_DATE('1984-06-12', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-1', '010-1234-1234', 'A', TO_DATE('2017-01-01', 'YYYY-MM-DD'))
+, ('0000000005', '안정환', '남자', TO_DATE('1971-07-04', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-2', '010-4321-4321', 'A', TO_DATE('2018-06-01', 'YYYY-MM-DD'))
+, ('0000000006', '고종수', '여자', TO_DATE('1994-12-28', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-3', '010-5678-5678', 'C', TO_DATE('2019-12-01', 'YYYY-MM-DD'))
+, ('0000000007', '기성용', '남자', TO_DATE('1984-06-12', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-1', '010-1234-1234', 'B', TO_DATE('2017-01-01', 'YYYY-MM-DD'))
+, ('0000000008', '이청용', '남자', TO_DATE('1971-07-04', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-2', '010-4321-4321', 'C', TO_DATE('2018-06-01', 'YYYY-MM-DD'))
+, ('0000000009', '박지성', '여자', TO_DATE('1994-12-28', 'YYYY-MM-DD'), '경기도 안양시 동안구 비산동 1-3', '010-5678-5678', 'D', TO_DATE('2019-12-01', 'YYYY-MM-DD'));
+
+-- 전체고객수, 등급의개수, 등급별평균고객수, 등급별최대고객수, 등급별최소고객수, 최소고객수의등급, 최대고객수의등급
+select
+count(*) "전체고객수",
+count(distinct CUSTOMER_GRADE) "등급의개수",
+round(max(AVG_BY_GRADE),2) "등급별평균고객수",
+max(MAX_BY_GRADE) "등급별최대고객수",
+max(MIN_BY_GRADE) "등급별최소고객수",
+max(GRADE_BY_MIN_EMP_COUNT) "최소고객수의등급",
+max(GRADE_BY_MAX_EMP_COUNT) "최대고객수의등급"
+from TB_MOVIE_CUST, -- 인라인 뷰 B,C,D는 모두 단 한 건만 리턴하므로 TB_MOVIE_CUST와 조인될 경우 데이터 건수의 변화가 없음. 해당 테이블로 전체 고객수 및 등그브이 개수를 구함
+	(select AVG(CNT) AVG_BY_GRADE, MAX(CNT) MAX_BY_GRADE, MIN(CNT) MIN_BY_GRADE -- 카운트 값을 기준으로 평균,최대,최소값을 구함
+		from (select count(*) CNT from TB_MOVIE_CUST A group by CUSTOMER_GRADE) A) -- TB_MOVIE_CUST 테이블을 CUSTOMER_GRADE 칼럼으로 group_by 한 고객등급 별 카운트를 구함
+	B,
+	(select CUSTOMER_GRADE as GRADE_BY_MIN_EMP_COUNT
+		from (select CUSTOMER_GRADE, count(*) CNT from TB_MOVIE_CUST -- 고객 등급별로 고객수가 가장 적은 하나의 등급을 구함 (asc 정렬)
+			group by CUSTOMER_GRADE order by CNT) A
+	limit 1)
+	C,
+	(select CUSTOMER_GRADE as GRADE_BY_MAX_EMP_COUNT
+		from (select CUSTOMER_GRADE, count(*) CNT from TB_MOVIE_CUST -- 고객 등급벼로 고객수가 가장 많은 하나의 등급을 구함 (desc 정렬)
+			group by CUSTOMER_GRADE order by CNT desc) A
+	limit 1)
+	D
+;
